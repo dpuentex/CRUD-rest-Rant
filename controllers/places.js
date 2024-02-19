28c4b15677825afcb75d97310dcbe5ba96753524
dpuentex <dpuente1097@gmail.com>
2024-02-19 01:28:18 -0800

    nav bar w style

--- controllers/places.js
+++ controllers/places.js
@@ -1,28 +1,13 @@
 const router = require("express").Router();
-
-router.get("/new", (req, res) => {
-  res.render("places/new");
-});
+const places = require("../models/places");
 
 // GET /places
 router.get("/", (req, res) => {
-  let places = [
-    {
-      name: "H-Thai-ML",
-      city: "Seattle",
-      state: "WA",
-      cuisines: "Thai, Pan-Asian",
-      pic: "https://media-cdn.tripadvisor.com/media/photo-s/10/14/c8/05/front-of-entrance-to.jpg",
-    },
-    {
-      name: "Coding Cat Cafe",
-      city: "Phoenix",
-      state: "AZ",
-      cuisines: "Coffee, Bakery",
-      pic: "https://catsherdyou.com/wp-content/uploads/2099/12/O_Cat_Cafe_000_wm.jpg",
-    },
-  ]; // Renders Places/Index
   res.render("places/index", { places });
+});
+
+router.get("/new", (req, res) => {
+  res.render("places/new");
 });
 
 router.post("/", (req, res) => {
